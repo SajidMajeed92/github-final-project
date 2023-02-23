1e1907b7aa8d2971bc954b65872d3b4c51d54622
Sajid Majeed <92437858+SajidMajeed92@users.noreply.github.com>
2023-02-23 22:45:55 +0500

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
    # This script calculates simple interest given principal,
    # annual rate of interest and time period in years.
-
    # Do not use this in production. Sample purpose only.
-
+   
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
    # <SajidMajeed92>
